432100ffe8e59427e6e8f5bf76e8aac5a7566888
Takatoshi Ono <takatoshi.ono@gmail.com>
2015-07-09 13:42:31 +0900

    Support cp932 and shift_jis

--- lib/wavedash.rb
+++ lib/wavedash.rb
@@ -21,6 +21,23 @@ module Wavedash
       "\u{FFE1}" => "\u{00A3}", # 'FULLWIDTH POUND SIGN' => 'POUND SIGN'
       "\u{FFE2}" => "\u{00AC}", # 'FULLWIDTH NOT SIGN' => 'NOT SIGN'
     },
+    'cp932' => {
+      "\u{301C}" => "\u{FF5E}", # 'WAVE DASH' => 'FULLWIDTH TILDE'
+      "\u{2212}" => "\u{FF0D}", # 'MINUS SIGN' => 'FULLWIDTH HYPHEN-MINUS'
+      "\u{2016}" => "\u{2225}", # 'DOUBLE VERTICAL LINE' => 'PARALLEL TO'
+      "\u{2014}" => "\u{2015}", # 'EM DASH' => 'HORIZONTAL BAR'
+      "\u{00A2}" => "\u{FFE0}", # 'CENT SIGN' => 'FULLWIDTH CENT SIGN'
+      "\u{00A3}" => "\u{FFE1}", # 'POUND SIGN' => 'FULLWIDTH POUND SIGN'
+      "\u{00AC}" => "\u{FFE2}", # 'NOT SIGN' => 'FULLWIDTH NOT SIGN'
+    },
+    'shift_jis' => {
+      "\u{FF5E}" => "\u{301C}", # 'FULLWIDTH TILDE' => 'WAVE DASH'
+      "\u{FF0D}" => "\u{2212}", # 'FULLWIDTH HYPHEN-MINUS' => 'MINUS SIGN'
+      "\u{2225}" => "\u{2016}", # 'PARALLEL TO' => 'DOUBLE VERTICAL LINE'
+      "\u{FFE0}" => "\u{00A2}", # 'FULLWIDTH CENT SIGN' => 'CENT SIGN'
+      "\u{FFE1}" => "\u{00A3}", # 'FULLWIDTH POUND SIGN' => 'POUND SIGN'
+      "\u{FFE2}" => "\u{00AC}", # 'FULLWIDTH NOT SIGN' => 'NOT SIGN'
+    },
   }
 
   def self.destination_encoding=(encoding)
--- spec/wavedash_spec.rb
+++ spec/wavedash_spec.rb
@@ -142,6 +142,126 @@ describe Wavedash do
         it_behaves_like 'a expected normalization'
       end
     end
+
+    context 'destination encoding is cp932' do
+      let(:encoding) { 'cp932' }
+
+      context 'includes some invalid characters' do
+        let(:str) { "こんにちは〜。コンニチハ−" }
+        let(:normalized_str) { "こんにちは～。コンニチハ－" }
+
+        it_behaves_like 'a unencodable string before-after'
+        it_behaves_like 'a expected normalization'
+      end
+
+      context 'includes WAVE DASH(U+301C)' do
+        let(:str) { "こんにちは〜" }
+        let(:normalized_str) { "こんにちは～" }
+
+        it_behaves_like 'a unencodable string before-after'
+        it_behaves_like 'a expected normalization'
+      end
+
+      context 'includes MINUS SIGN(U+2212)' do
+        let(:str) { "−" }
+        let(:normalized_str) { "－" }
+
+        it_behaves_like 'a unencodable string before-after'
+        it_behaves_like 'a expected normalization'
+      end
+
+      context 'includes DOUBLE VERTICAL LINE(U+2016)' do
+        let(:str) { "‖" }
+        let(:normalized_str) { "∥" }
+
+        it_behaves_like 'a unencodable string before-after'
+        it_behaves_like 'a expected normalization'
+      end
+
+      context 'includes EM DASH(U+2014)' do
+        let(:str) { "—" }
+        let(:normalized_str) { "―" }
+
+        it_behaves_like 'a unencodable string before-after'
+        it_behaves_like 'a expected normalization'
+      end
+
+      context 'includes CENT SIGN(U+00A2)' do
+        let(:str) { "¢" }
+        let(:normalized_str) { "￠" }
+
+        it_behaves_like 'a unencodable string before-after'
+        it_behaves_like 'a expected normalization'
+      end
+
+      context 'includes POUND SIGN(U+00A3)' do
+        let(:str) { "£" }
+        let(:normalized_str) { "￡" }
+
+        it_behaves_like 'a unencodable string before-after'
+        it_behaves_like 'a expected normalization'
+      end
+
+      context 'includes NOT SIGN(U+00AC)' do
+        let(:str) { "¬" }
+        let(:normalized_str) { "￢" }
+
+        it_behaves_like 'a unencodable string before-after'
+        it_behaves_like 'a expected normalization'
+      end
+    end
+
+    context 'destination encoding is shift_jis' do
+      let(:encoding) { 'shift_jis' }
+
+      context 'includes FULLWIDTH TILDE(U+FF5E)' do
+        let(:str) { "こんにちは～" }
+        let(:normalized_str) { "こんにちは〜" }
+
+        it_behaves_like 'a unencodable string before-after'
+        it_behaves_like 'a expected normalization'
+      end
+
+      context 'includes FULLWIDTH HYPHEN-MINUS(U+FF0D)' do
+        let(:str) { "－" }
+        let(:normalized_str) { "−" }
+
+        it_behaves_like 'a unencodable string before-after'
+        it_behaves_like 'a expected normalization'
+      end
+
+      context 'includes PARALLEL TO(U+2225)' do
+        let(:str) { "∥" }
+        let(:normalized_str) { "‖" }
+
+        it_behaves_like 'a unencodable string before-after'
+        it_behaves_like 'a expected normalization'
+      end
+
+      context 'includes FULLWIDTH CENT SIGN(U+FFE0)' do
+        let(:str) { "￠" }
+        let(:normalized_str) { "¢" }
+
+        it_behaves_like 'a unencodable string before-after'
+        it_behaves_like 'a expected normalization'
+      end
+
+      context 'includes FULLWIDTH POUND SIGN(U+FFE1)' do
+        let(:str) { "￡" }
+        let(:normalized_str) { "£" }
+
+        it_behaves_like 'a unencodable string before-after'
+        it_behaves_like 'a expected normalization'
+      end
+
+      context 'includes FULLWIDTH NOT SIGN(U+FFE2)' do
+        let(:str) { "￢" }
+        let(:normalized_str) { "¬" }
+
+        it_behaves_like 'a unencodable string before-after'
+        it_behaves_like 'a expected normalization'
+      end
+    end
   end
 
   describe '#invalid?' do
@@ -196,6 +316,66 @@ describe Wavedash do
 
     context 'destination encoding is euc-jp' do
       let(:encoding) { 'euc-jp' }
+
+      it 'returns false when not invalid characters' do
+        expect(Wavedash.invalid?('こんにちは')).to be_falsey
+      end
+
+      it 'returns true when include FULLWIDTH TILDE(U+FF5E)' do
+        expect(Wavedash.invalid?("こんにちは～")).to be_truthy
+      end
+
+      it 'returns true when include PARALLEL TO(U+2225)' do
+        expect(Wavedash.invalid?("∥")).to be_truthy
+      end
+
+      it 'returns true when include FULLWIDTH CENT SIGN(U+FFE0)' do
+        expect(Wavedash.invalid?("￠")).to be_truthy
+      end
+
+      it 'returns true when include FULLWIDTH POUND SIGN(0+FFE1)' do
+        expect(Wavedash.invalid?("￡")).to be_truthy
+      end
+
+      it 'returns true when include FULLWIDTH NOT SIGN(0+FFE2)' do
+        expect(Wavedash.invalid?("￢")).to be_truthy
+      end
+    end
+
+    context 'destination encoding is cp932' do
+      let(:encoding) { 'cp932' }
+
+      it 'returns false when not invalid characters' do
+        expect(Wavedash.invalid?('こんにちは')).to be_falsey
+      end
+
+      it 'returns true when include WAVE DASH(U+301C)' do
+        expect(Wavedash.invalid?("こんにちは〜")).to be_truthy
+      end
+
+      it 'returns true when include DOUBLE VERTICAL LINE(U+2016)' do
+        expect(Wavedash.invalid?("‖")).to be_truthy
+      end
+
+      it 'returns true when include EM DASH(U+2014)' do
+        expect(Wavedash.invalid?("—")).to be_truthy
+      end
+
+      it 'returns true when include CENT SIGN(U+00A2)' do
+        expect(Wavedash.invalid?("¢")).to be_truthy
+      end
+
+      it 'returns true when include POUND SIGN(U+00A3)' do
+        expect(Wavedash.invalid?("£")).to be_truthy
+      end
+
+      it 'returns true when include NOT SIGN(U+00AC)' do
+        expect(Wavedash.invalid?("¬")).to be_truthy
+      end
+    end
+
+    context 'destination encoding is shift_jis' do
+      let(:encoding) { 'shift_jis' }
 
       it 'returns false when not invalid characters' do
         expect(Wavedash.invalid?('こんにちは')).to be_falsey
